29442327d8dc0ce8c51c13bdaa7cf7a33172241d
Zachary Langley <zaclangley04@gmail.com>
2020-11-03 10:30:29 -0600

    nutrient recipe actions, reducers, types skeleton

--- client/src/domain/reducers/index.js
+++ client/src/domain/reducers/index.js
@@ -3,10 +3,14 @@ import { connectRouter } from 'connected-react-router'
 import auth from './authReducer';
 import app from './appReducer';
 import garden from './gardenReducer';
+import plant from './plantReducer';
+import nutrientRecipe from './nutrientRecipeReducer';
 
 export default (history) => combineReducers({
     app,
     auth,
     garden,
+    nutrientRecipe,
+    plant,
     router: connectRouter(history)
 });
